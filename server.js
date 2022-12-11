90d18bcba41f7e0bf604aab3ba7206da47afa78f
Tanmay <tanmayrsm@gmail.com>
2022-12-11 22:21:13 +0530

    intermediate changes for group call

--- server.js
+++ server.js
@@ -19,9 +19,11 @@ server.listen(PORT, () => {
 })
 
 app.use(cors());
-// app.get("/", (req, res, next) => {
-//     res.json(["Tony","Lisa","Michael","Ginger","Food"]);
-// });
+
+const users = {};
+
+const socketToRoom = {};
+
 io.on("connection", (socket) => {
 	
 	storeSocket = socket;
@@ -36,6 +38,51 @@ io.on("connection", (socket) => {
 	socket.on("answerCall", (data) => {
 		io.to(data.to).emit("callAccepted", data.signal)
 	})
+
+	socket.on('join-room', (roomId, userId) => {
+		console.log(`Socket ${socket.id} joining ${roomId}`);
+		socket.join(roomId);
+		socket.to(roomId).broadcast.emit('user-connected', userId);
+	});
+
+	socket.on("join room", roomID => {
+        if (users[roomID]) {
+            const length = users[roomID].length;
+            if (length === 4) {
+                socket.emit("room full");
+                return;
+            }
+            users[roomID].push(socket.id);
+        } else {
+            users[roomID] = [socket.id];
+        }
+        socketToRoom[socket.id] = roomID;
+        const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
+        socket.emit("all users", usersInThisRoom);
+    });
+
+    socket.on("sending signal", payload => {
+        io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
+    });
+
+    socket.on("returning signal", payload => {
+        io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
+    });
+
+    socket.on('disconnect', () => {
+        const roomID = socketToRoom[socket.id];
+        let room = users[roomID];
+        if (room) {
+            room = room.filter(id => id !== socket.id);
+            users[roomID] = room;
+        }
+        socket.broadcast.emit('user left',socket.id)
+    });
+
+    socket.on('change', (payload) => {
+        socket.broadcast.emit('change',payload)
+    });
+
 });
 app.get(`/establish`, (req, res, next) => {
 	if(storeSocket) {
