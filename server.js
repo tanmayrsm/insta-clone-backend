1a9fe5a0d9cd4448e6291d33cbf2ca1d72462946
Tanmay <tanmayrsm@gmail.com>
2022-12-31 21:27:48 +0530

    fix for new user not showing up added

--- server.js
+++ server.js
@@ -46,6 +46,7 @@ io.on("connection", (socket) => {
 	});
 
 	socket.on("join room", roomID => {
+        console.log("Someone joined ::", " ::  usrs :: ", users);
         if (users[roomID]) {
             const length = users[roomID].length;
             if (length === 4) {
@@ -58,17 +59,20 @@ io.on("connection", (socket) => {
         }
         socketToRoom[socket.id] = roomID;
         const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
+
+        // 4- emit all users in this room
         socket.emit("all users", usersInThisRoom);
     });
 
+    // 7- listen CREATED Peer from frontend
     socket.on("sending signal", payload => {
-        setTimeout(() => {
-            io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
-            console.log("User joined :: ",payload.callerID );
-        }, 5000)    // this time must increase as oer no of users for now
+        // 8- user joined process complete
+        io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
+        console.log("User joined :: ",payload.callerID );    
     });
 
     socket.on("returning signal", payload => {
+        // 12- ACK emit after receiving signal of newly added Peer been added to Peer list
         io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
     });
 
