8e76633951764e910ed05141f554dc30d4f9859b
Tanmay <tanmayrsm@gmail.com>
2023-01-07 20:33:49 +0530

    added call end feature

--- server.js
+++ server.js
@@ -24,6 +24,8 @@ const users = {};
 
 const socketToRoom = {};
 
+const userPersonalInfo = {};
+
 io.on("connection", (socket) => {
 	
 	storeSocket = socket;
@@ -45,7 +47,7 @@ io.on("connection", (socket) => {
 		socket.to(roomId).broadcast.emit('user-connected', userId);
 	});
 
-	socket.on("join room", roomID => {
+	socket.on("join room", ({roomID, user}) => {
         console.log("Someone joined ::", " ::  usrs :: ", users);
         if (users[roomID]) {
             const length = users[roomID].length;
@@ -54,26 +56,37 @@ io.on("connection", (socket) => {
                 return;
             }
             users[roomID].push(socket.id);
+            userPersonalInfo[roomID].push({[socket.id]: user});
         } else {
             users[roomID] = [socket.id];
+            userPersonalInfo[roomID] = [{[socket.id]: user}];
         }
         socketToRoom[socket.id] = roomID;
         const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
 
         // 4- emit all users in this room
         socket.emit("all users", usersInThisRoom);
+        socket.emit("all full users", userPersonalInfo[roomID]);
     });
 
     // 7- listen CREATED Peer from frontend
     socket.on("sending signal", payload => {
         // 8- user joined process complete
         io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
-        console.log("User joined :: ",payload.callerID );    
+        console.log("User joined :: ",payload.callerID );
+        const roomID = socketToRoom[socket.id];
+        socket.emit("all full users", userPersonalInfo[roomID]);    
     });
 
     socket.on("returning signal", payload => {
         // 12- ACK emit after receiving signal of newly added Peer been added to Peer list
         io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
+        const roomID = socketToRoom[socket.id];
+        socket.emit("all full users", userPersonalInfo[roomID]);
+    });
+
+    socket.on('disconnect socket', () => {
+        socket.disconnect();
     });
 
     socket.on('disconnect', () => {
@@ -83,11 +96,19 @@ io.on("connection", (socket) => {
             room = room.filter(id => id !== socket.id);
             users[roomID] = room;
         }
-        socket.broadcast.emit('user left',socket.id)
+        console.log("User leaving ::", socket.id);
+        socket.broadcast.emit('user left',socket.id);
+        if(userPersonalInfo && userPersonalInfo[roomID]) {
+            userPersonalInfo[roomID] = userPersonalInfo[roomID].filter(item => Object.keys(item)[0] !== socket.id);
+            console.log('other userPersonaInfo ::',userPersonalInfo[roomID]);
+            socket.emit("all full users", userPersonalInfo[roomID]);
+        }
     });
 
     socket.on('change', (payload) => {
-        socket.broadcast.emit('change',payload)
+        socket.broadcast.emit('change',payload);
+        const roomID = socketToRoom[socket.id];
+        socket.emit("all full users", userPersonalInfo[roomID]);
     });
 
 });
@@ -97,4 +118,41 @@ app.get(`/establish`, (req, res, next) => {
 		// console.log("id emit :: ", storeSocket.id);
 	}
 	return res.json("ok emitted");	
-})
+});
+
+
+// - call
+//     - from
+//         - ramUID
+//             - roomID
+//             - WHO all have currently joined : [ramUID]
+//             - roomOwner : ramUID
+//     - to
+//         - dushtaUID  ...... ur app will always listen this...modal will pop up, in case it activates
+//             - roomID
+//             - WHO all.. : [ramUID]
+//             - who calling : ramUID
+        
+// -----------------------------------------------------------------------
+
+// cases - 
+
+// - dushta picks call -  
+//     1- if dushta picks the call -> to.dushtaUID will be deleted
+//     2- dushtaUID will be added in from.ramUID.whoallInCall..[]
+//     3- once dushta Leaves call ->
+//         - he will leave from from.ramUID.whoallInCall
+//         - his chat with ram, and vice versa - will have an Audio call entry, with call ended time
+//         - if it was call wid only two participants -> call will also end for other guy
+//         - call from and to will be deleted
+
+// - dushta doesn't picks call, or ends call - 
+//     1- if dushtas receiving call since 90 secs...automatically, call to him must be hanged up
+//         - again if it was one to one -> call must end even for ram
+//         - else nothing much for now 
+//     2- during removal of to.dushta
+//         - after 90 secs - add missed call for dushta as message...and small btn to call ram back
+//         - if he cancels call (on modal) - add same audio call cancelled message for both ram and dushta
+
+
+
